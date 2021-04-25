f9f83e29757a461af55e1ce07888fadb38951265
mertyakdemir <mertyakdemir07@gmail.com>
2021-04-25 19:13:40 +0300

    commit

---
+++ RedisExchangeAPI.Web/Controllers/HashTypeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Web.Services;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisExchangeAPI.Web.Controllers
+{
+    public class HashTypeController : Controller
+    {
+        private readonly RedisService _redisService;
+
+        private readonly IDatabase db;
+
+        public string hashKey { get; set; } = "hashType";
+
+        public HashTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+            db = _redisService.GetDb(4);
+        }
+
+        public IActionResult Index()
+        {
+            Dictionary<string, string> list = new Dictionary<string, string>();
+
+            if (db.KeyExists(hashKey))
+            {
+                db.HashGetAll(hashKey).ToList().ForEach(x =>
+                {
+                    list.Add(x.Name, x.Value);
+                });
+            }
+
+            return View(list);
+        }
+
+        [HttpPost]
+        public IActionResult Add(string name, string val)
+        {
+            db.HashSet(hashKey, name, val);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteItem(string name)
+        {
+            db.HashDelete(hashKey, name);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ RedisExchangeAPI.Web/Controllers/StringTypeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Web.Services;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisExchangeAPI.Web.Controllers
+{
+    public class StringTypeController : Controller
+    {
+        private readonly RedisService _redisService;
+        private readonly IDatabase db;
+
+        public StringTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+            db = _redisService.GetDb(0);
+        }
+
+        public IActionResult Index()
+        {
+            var db = _redisService.GetDb(0);
+            db.StringSet("name", "Jack");
+            db.StringSet("visitors", 1000);
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            var value = db.StringGet("name");
+
+            //db.StringIncrement("visitors", 10);
+
+            var count = db.StringDecrementAsync("visitors", 1).Result;
+
+            if (value.HasValue)
+            {
+                ViewBag.Name = value.ToString();
+            }
+
+            return View();
+        }
+    }
+}
